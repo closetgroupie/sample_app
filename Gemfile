59e6fa911d42ca7f2d406d8b406085799d14f668
closetgroupie <kjames1581@gmail.com>
2014-10-29 13:08:21 -0600

    finish user edit

--- Gemfile
+++ Gemfile
@@ -12,6 +12,10 @@ gem 'jbuilder',             '2.1.3'
 gem 'rails-html-sanitizer', '1.0.1'
 gem 'sdoc',                 '0.4.0', group: :doc
 
+gem 'faker',                   '1.4.2'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
+
 
 
 #facebook integration
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -8,14 +11,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  
   def create
     @user = User.new(user_params)
-    if @user.save   # Handle a successful save.
+    if @user.save
       log_in @user
-       flash[:success] = "welcome to sewingly!"
-       redirect_to '/'
-      
+      flash[:success] = "welcome to sewingly!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -25,10 +26,42 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+# Before filters
+
+ # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
+
+
+
+
+
+
