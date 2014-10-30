4047d06a3919d25d44d1395f571ea639bc5fe552
closetgroupie <kjames1581@gmail.com>
2014-10-30 14:17:04 -0600

    Add account activation, password resets & email configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "welcome to sewingly!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your sewingly account."
+      redirect_to root_url
     else
       render 'new'
     end
