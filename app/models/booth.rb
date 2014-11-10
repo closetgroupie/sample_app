2088192a1716c5d278a0b15a086a6f5619be7c48
closetgroupie <kjames1581@gmail.com>
2014-11-10 16:50:27 -0600

    add remember token to booth.rb

--- app/models/booth.rb
+++ app/models/booth.rb
@@ -2,4 +2,7 @@ class Booth < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 25}
+
+  attr_accessor :remember_token
+  
 end
