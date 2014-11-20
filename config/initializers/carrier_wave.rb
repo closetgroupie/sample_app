4173e3a3befb7f5f58f91b1f0456f03cf65a42d8
closetgroupie <kjames1581@gmail.com>
2014-11-20 12:09:21 -0600

    fix access keys in carrierwave config initializer

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJHS3DDVVPP2TBMDQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_AKIAJHS3DDVVPP2TBMDQ'],
+      :aws_secret_access_key => ENV['S3_YnL9w54+RgZkGv16G4AIX8cmkrZP7S/I/kTpu1tm']
     }
-    config.fog_directory     =  ENV['sewingly']
+    config.fog_directory     =  ENV['S3_sewingly']
   end
 end
 
